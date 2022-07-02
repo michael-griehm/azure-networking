14075bd24cf5572617a3fd530b69dad1459da517
Michael Griehm <griehmmj@gmail.com>
2022-07-02 12:23:46 -0400

    Fixed `private_dns_zone_name` values

--- iac/workload/private-dns-zone.tf
+++ iac/workload/private-dns-zone.tf
@@ -6,21 +6,21 @@ resource "azurerm_private_dns_zone" "blobs" {
 resource "azurerm_private_dns_zone_virtual_network_link" "hub_blob_link" {
   name                  = "hub-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_blobs.name
+  private_dns_zone_name = azurerm_private_dns_zone.blobs.name
   virtual_network_id    = azurerm_virtual_network.hub.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_databricks_spoke_blob_link" {
   name                  = "delta-lakehouse-databricks-spoke-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_blobs.name
+  private_dns_zone_name = azurerm_private_dns_zone.blobs.name
   virtual_network_id    = azurerm_virtual_network.delta_lakehouse_databricks_spoke.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_storage_spoke_blob_link" {
   name                  = "delta-lakehouse-storage-spoke-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_blobs.name
+  private_dns_zone_name = azurerm_private_dns_zone.blobs.name
   virtual_network_id    = azurerm_virtual_network.delta_lakehouse_storage_spoke.id
 }
 
@@ -32,20 +32,20 @@ resource "azurerm_private_dns_zone" "dfs" {
 resource "azurerm_private_dns_zone_virtual_network_link" "hub_dfs_link" {
   name                  = "hub-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  private_dns_zone_name = azurerm_private_dns_zone.dfs.name
   virtual_network_id    = azurerm_virtual_network.hub.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_databricks_spoke_dfs_link" {
   name                  = "delta-lakehouse-databricks-spoke-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  private_dns_zone_name = azurerm_private_dns_zone.dfs.name
   virtual_network_id    = azurerm_virtual_network.delta_lakehouse_databricks_spoke.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_storage_spoke_dfs_link" {
   name                  = "delta-lakehouse-storage-spoke-link"
   resource_group_name   = data.azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  private_dns_zone_name = azurerm_private_dns_zone.dfs.name
   virtual_network_id    = azurerm_virtual_network.delta_lakehouse_storage_spoke.id
 }
