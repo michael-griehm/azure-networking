d2f557f30e911e5ae26306ad23e97f4dc6b7cec5
Michael Griehm <griehmmj@gmail.com>
2022-07-02 11:51:47 -0400

    Added private dns zones for Storage Account Blob DFS.

--- iac/workload/private-dns-zone.tf
+++ iac/workload/private-dns-zone.tf
@@ -23,3 +23,29 @@ resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_storag
   private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_blobs.name
   virtual_network_id    = azurerm_virtual_network.delta_lakehouse_storage_spoke.id
 }
+
+resource "azurerm_private_dns_zone" "delta_lakehouse_dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "hub_link" {
+  name                  = "hub-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  virtual_network_id    = azurerm_virtual_network.hub.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_databricks_spoke_link" {
+  name                  = "delta-lakehouse-databricks-spoke-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  virtual_network_id    = azurerm_virtual_network.delta_lakehouse_databricks_spoke.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "delta_lakehouse_storage_spoke_link" {
+  name                  = "delta-lakehouse-storage-spoke-link"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.delta_lakehouse_dfs.name
+  virtual_network_id    = azurerm_virtual_network.delta_lakehouse_storage_spoke.id
+}
