6ef23c9c286f48f1b9c48d5153f6396ae261546c
Michael Griehm <griehmmj@gmail.com>
2022-07-11 16:34:42 -0400

    Added ability to forward traffic, etc in peering

--- iac/workload/hub.tf
+++ iac/workload/hub.tf
@@ -7,15 +7,21 @@ resource "azurerm_virtual_network" "hub" {
 }
 
 resource "azurerm_virtual_network_peering" "hub_to_delta_lakehouse_spoke" {
-  name                      = "hub-to-delta-lakehouse-spoke"
-  resource_group_name       = data.azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.hub.name
-  remote_virtual_network_id = azurerm_virtual_network.delta_lakehouse_spoke.id
+  name                         = "hub-to-delta-lakehouse-spoke"
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.delta_lakehouse_spoke.id
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+  allow_virtual_network_access = true
 }
 
 resource "azurerm_virtual_network_peering" "delta_lakehouse_spoke_to_hub" {
-  name                      = "delta-lakehouse-spoke-to-hub"
-  resource_group_name       = data.azurerm_resource_group.rg.name
-  virtual_network_name      = azurerm_virtual_network.delta_lakehouse_spoke.name
-  remote_virtual_network_id = azurerm_virtual_network.hub.id
+  name                         = "delta-lakehouse-spoke-to-hub"
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  virtual_network_name         = azurerm_virtual_network.delta_lakehouse_spoke.name
+  remote_virtual_network_id    = azurerm_virtual_network.hub.id
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+  allow_virtual_network_access = true
 }
